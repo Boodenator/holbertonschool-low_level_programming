caf69fe13d946900958294c551a375d64e623f80
Boodenator <boode1000@gmail.com>
2025-03-04 20:50:40 +0000

    hexa

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -2,18 +2,16 @@
 /**
  * main - prints all the numbers of base 16 in lowercase,
  * followed by a new line
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = '0'; c <= '9'; c++)
-        putchar(c);
-    for (c = 'a'; c <= 'f'; c++)
-        putchar(c);
-
-    putchar('\n');
-
-    return (0);
+	for (c = '0'; c < '9'; c++)
+		putchar(c);
+	for (c = 'a'; c < 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
