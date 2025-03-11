fa433e53e41cbc6108b9f7a517f8cee6ee0f4a1c
Boodenator <boode1000@gmail.com>
2025-03-11 20:00:14 +0000

    return length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = [];
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
