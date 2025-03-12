cea0bbc7cec793ef25901aeffca57320c744c938
Boodenator <boode1000@gmail.com>
2025-03-12 13:22:29 +0000

    print string in rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	for(int j = i - 1; j >= 0; j--)
+	{
+	putchar(s[j]);
+	}
+putchar('\n');
+}
