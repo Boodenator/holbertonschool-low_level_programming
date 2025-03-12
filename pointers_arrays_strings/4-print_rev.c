2250cab1df11c85c13add3a50739b3d69ce45c52
Boodenator <boode1000@gmail.com>
2025-03-12 13:28:20 +0000

    rev string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,12 @@ void print_rev(char *s)
 {
 	int i = 0;
 	int j;
-	while(s[i] != '\0')
+
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	for(int j = i - 1; j >= 0; j--)
+	for (int j = i - 1; j >= 0; j--)
 	{
 	putchar(s[j]);
 	}
