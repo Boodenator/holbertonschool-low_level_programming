bada1290050e65b46bb1cc27221a58252383dd78
Boodenator <boode1000@gmail.com>
2025-04-12 11:02:52 +0000

    sum of all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,14 +8,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    if (n == 0)
-        return 0;
-
-    va_list args;
+	unsigned int i = 0;
+	int sum = 0;
+	va_list args;
     va_start(args, n);
 
-    int sum = 0;
-    unsigned int i = 0;
+    if (n == 0)
+        return 0;
     for (i = 0; i < n; i ++)
     {
 	    sum += va_arg(args, int);
