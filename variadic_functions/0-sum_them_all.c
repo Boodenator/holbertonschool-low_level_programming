00eab51353e6dee39e28b598a3aed5b0d63eae73
Boodenator <boode1000@gmail.com>
2025-04-12 11:22:38 +0000

    print numbers

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,9 +11,9 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i = 0;
 	int sum = 0;
 	va_list args;
-    va_start(args, n);
+   	va_start(args, n);
 
-    if (n == 0)
+   if (n == 0)
         return 0;
     for (i = 0; i < n; i ++)
     {
---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
