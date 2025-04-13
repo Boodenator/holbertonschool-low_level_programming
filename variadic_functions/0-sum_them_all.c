f340c4410b896ad5ce03fc067f49e9139165aed2
Boodenator <boode1000@gmail.com>
2025-04-13 21:45:32 +0200

    print numbers

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-
 /**
  * sum_them_all - calculates the sum of all its parameters
  * @n: number of arguments passed to the function
@@ -8,16 +7,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i = 0;
-	int sum = 0;
-	va_list args;
-   	va_start(args, n);
+   unsigned int i = 0;
+    int sum = 0;
+    va_list args;
 
-   if (n == 0)
+    va_start(args, n);
+
+    if (n == 0)
         return 0;
-    for (i = 0; i < n; i ++)
+
+    for (i = 0; i < n; i++)
     {
-	    sum += va_arg(args, int);
+        sum += va_arg(args, int);
     }
     va_end(args);
     return (sum);
--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -7,17 +7,22 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i = 0;
-	va_list args;
-	va_start(args, n);
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL && i < n - 1)
-		{
-			printf("%s", separator);
-		}
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
 	}
-	va_end(args);
+
+	va_end(list);
+
 	printf("\n");
 }
